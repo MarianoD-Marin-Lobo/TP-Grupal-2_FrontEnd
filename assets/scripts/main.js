af130af6dcfc4e86c1d00f269a825298b58c1c8b
MarianoD-Marin-Lobo <mdmarin2@gmail.com>
2024-11-23 23:52:11 -0300

    Se añaden las funciones para mostrar maximo numero de un arreglo y para calcular la media de un arreglo de numeros. Se adicionan ejemplos para probar el funcionamiento de dichas funciones desde Main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,7 @@
 import {contarVocales} from './functionsScripts/contarVocales.js';
 // ---------
 
-// import {maxNum, calcularMedia} from './functionsScripts/mayorNroMediaArray.js';
+import {maxNum, calcularMedia} from './functionsScripts/mayorNroMediaArray.js';
 // ---------
 
 import {invertirArray, eliminarDuplicados} from './functionsScripts/invertElemElimDuplic.js';
@@ -35,22 +35,50 @@ printSeparador();
 // ---------------------------------------------------------------------------------------------------
 
 
-// Ejercicio-2
-// const arrayMaxNroA = [0, 1, 1, 2, 3, 5, 81, 13, 21, 34, 55, 89];
-// console.log(`Solución-2: Muestra el mayor número de un array.`);
-// console.log(`Array ingresado ---> "${arrayMaxNro}"`);
-// console.log(`Mayor número ---> ${maxNum(maxNro)}`);
-// printSeparador();
+Ejercicio-2
+const arrayMaxNroA = [0, 1, 1, 2, 3, 5, 81, 13, 21, 34, 55, 89];
+const arrayMaxNroB = [5, 12, 72, 24, 98, 1, 33, 67];
+const arrayMaxNroC = [44, 8, 15, 27, 74, 128, 52, 29, 126];
+
+console.log(`Solución-2: Muestra el mayor número de un array.`);
+
+console.log('---> Ejemplo 1:');
+console.log(`Array ingresado ---> "${arrayMaxNroA}"`);
+console.log(`Mayor número ---> ${maxNum(arrayMaxNroA)}`);
+
+console.log('---> Ejemplo 2:');
+console.log(`Array ingresado ---> "${arrayMaxNroB}"`);
+console.log(`Mayor número ---> ${maxNum(arrayMaxNroB)}`);
+
+console.log('---> Ejemplo 3:');
+console.log(`Array ingresado ---> "${arrayMaxNroC}"`);
+console.log(`Mayor número ---> ${maxNum(arrayMaxNroC)}`);
+
+printSeparador();
 
 // ---------------------------------------------------------------------------------------------------
 
 
-// Ejercicio-3
-// const arrayMedia = [10, 20, 30, 40, 50];
-// console.log(`Solución-3: Calcular la media de un array de números.`);
-// console.log(`Array ingresado ---> "${arrayMedia}"`);
-// console.log(`Valor MEDIO del array ---> ${calcularMedia(arrayMedia)}`);
-// printSeparador();
+Ejercicio-3
+const arrayMediaA = [10, 20, 30, 40, 50];
+const arrayMediaB = [18, 21, 24, 27, 30];
+const arrayMediaC = [5, 15, 25, 35, 45];
+
+console.log(`Solución-3: Calcular la media de un array de números.`);
+
+console.log('---> Ejemplo 1:');
+console.log(`Array ingresado ---> "${arrayMediaA}"`);
+console.log(`Valor MEDIO del array ---> ${calcularMedia(arrayMediaA)}`);
+
+console.log('---> Ejemplo 2:');
+console.log(`Array ingresado ---> "${arrayMediaB}"`);
+console.log(`Valor MEDIO del array ---> ${calcularMedia(arrayMediaB)}`);
+
+console.log('---> Ejemplo 3:');
+console.log(`Array ingresado ---> "${arrayMediaC}"`);
+console.log(`Valor MEDIO del array ---> ${calcularMedia(arrayMediaC)}`);
+
+printSeparador();
 
 
 // ---------------------------------------------------------------------------------------------------
@@ -63,15 +91,15 @@ const arrayAInvertirC = [true, false, true];
 
 console.log(`Solución-4: Invertir los elementos de un array.`);
 
-console.log('---> Ejemplo 1:')
+console.log('---> Ejemplo 1:');
 console.log(`Array ingresado ---> "${arrayAInvertirA}"`);
 console.log(`Array invertido ---> ${invertirArray(arrayAInvertirA)}`); // Resultado: [5, 4, 3, 2, 1]
 
-console.log('---> Ejemplo 2:')
+console.log('---> Ejemplo 2:');
 console.log(`Array ingresado ---> "${arrayAInvertirB}"`);
 console.log(`Array invertido ---> ${invertirArray(arrayAInvertirB)}`); // Resultado: ['c', 'b', 'a']
 
-console.log('---> Ejemplo 3:')
+console.log('---> Ejemplo 3:');
 console.log(`Array ingresado ---> "${arrayAInvertirC}"`);
 console.log(`Array invertido ---> ${invertirArray(arrayAInvertirC)}`); // Resultado: [true, false, true]
 printSeparador();
@@ -87,17 +115,17 @@ const arrayConDuplicadosC = [true, true, false, false];
 
 console.log(`Solución-5: Eliminar elementos duplicados de un array.`);
 
-console.log('---> Ejemplo 1:')
+console.log('---> Ejemplo 1:');
 console.log(`Array ingresado ---> "${arrayConDuplicadosA}"`);
 console.log(`Array sin elementos duplicados ---> ${eliminarDuplicados(arrayConDuplicadosA)}`); 
 // Resultado: [1, 2, 3, 4, 5]
 
-console.log('---> Ejemplo 2:')
+console.log('---> Ejemplo 2:');
 console.log(`Array ingresado ---> "${arrayConDuplicadosB}"`);
 console.log(`Array sin elementos duplicados ---> ${eliminarDuplicados(arrayConDuplicadosB)}`);
 // Resultado: ['a', 'b', 'c']
 
-console.log('---> Ejemplo 3:')
+console.log('---> Ejemplo 3:');
 console.log(`Array ingresado ---> "${arrayConDuplicadosC}"`);
 console.log(`Array sin elementos duplicados ---> ${eliminarDuplicados(arrayConDuplicadosC)}`);
 // Resultado: [true, false]
@@ -115,15 +143,15 @@ const cadenaPruebaCapitC = "i'll spit on your grave";
 
 console.log(`Solución-6: Convertir la primera letra de cada palabra en mayúscula.`);
 
-console.log('---> Ejemplo 1:')
+console.log('---> Ejemplo 1:');
 console.log(`Oración original ---> "${cadenaPruebaCapitA}"`);
 console.log(`Oración con palabras capitalizadas ---> ${capitalizarPalabras(cadenaPruebaCapitA)}`);
 
-console.log('---> Ejemplo 2:')
+console.log('---> Ejemplo 2:');
 console.log(`Oración original ---> "${cadenaPruebaCapitB}"`);
 console.log(`Oración con palabras capitalizadas ---> ${capitalizarPalabras(cadenaPruebaCapitB)}`);
 
-console.log('---> Ejemplo 3:')
+console.log('---> Ejemplo 3:');
 console.log(`Oración original ---> "${cadenaPruebaCapitC}"`);
 console.log(`Oración con palabras capitalizadas ---> ${capitalizarPalabras(cadenaPruebaCapitC)}`);
 
@@ -141,15 +169,15 @@ const cantTermFibonacciC = 10;
 
 console.log(`Solución-7: Generar los primeros N números de la sucesión de Fibonacci.`);
 
-console.log('---> Ejemplo 1:')
+console.log('---> Ejemplo 1:');
 console.log(`Cant. de Términos requerida ---> ${cantTermFibonacciA}`);
 fibonacci(cantTermFibonacciA);
 
-console.log('---> Ejemplo 2:')
+console.log('---> Ejemplo 2:');
 console.log(`Cant. de Términos requerida ---> ${cantTermFibonacciB}`);
 fibonacci(cantTermFibonacciB);
 
-console.log('---> Ejemplo 3:')
+console.log('---> Ejemplo 3:');
 console.log(`Cant. de Términos requerida ---> ${cantTermFibonacciC}`);
 fibonacci(cantTermFibonacciC);
 
